a45b3db3c5fc1e05a27d839edcb0f37380b49d7a
Cool <dadavandrej@gmail.com>
2024-08-25 00:12:17 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using StreamReader reader = new StreamReader(args[0]);
+Interpret(reader.ReadToEnd());
+Console.Read();
+
+static void Interpret(string code)
+{
+    var m = new byte[30000];
+    int p = 0, l = code.Length;
+    for (int i = 0; i < l; i++)
+    {
+        switch (code[i])
+        {
+            case '>': p++; break;
+            case '<': p--; break;
+            case '+': m[p]++; break;
+            case '-': m[p]--; break;
+            case '.': Console.Write((char)m[p]); break;
+            case ',': m[p] = (byte)Console.Read(); break;
+            case '[': if (m[p] == 0) i = code.IndexOf(']', i); break;
+            case ']': if (m[p] != 0) i = code.LastIndexOf('[', i); break;
+        }
+    }
+}
